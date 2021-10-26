3082e61dfbb06f02c95b06990a247f2212c63ed8
Virginia <virginiapiazza19@gmail.com>
2021-10-26 11:52:54 -0300

    Se agrega método getAtraccionDAO()

--- src/main/java/dao/DAOFactory.java
+++ src/main/java/dao/DAOFactory.java
@@ -1,8 +1,12 @@
 package dao;
 
-	public class DAOFactory {
+public class DAOFactory {
 
-		public static UsuarioDAO getUsuarioDAO() {
-			return new UsuarioDAOImpl();
-		}
+	public static UsuarioDAO getUsuarioDAO() {
+		return new UsuarioDAOImpl();
 	}
+
+	public static AtraccionDAO getAtraccionDAO() {
+		return new AtraccionDAOImpl();
+	}
+}
