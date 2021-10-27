435ce1bb6428459dfa18d4db3b33a5c5aaa80dad
Virginia <virginiapiazza19@gmail.com>
2021-10-26 22:12:00 -0300

    Saqué las pruebas de insertar y eliminar

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -3,8 +3,7 @@ package app;
 import dao.AtraccionDAO;
 import dao.DAOFactory;
 import dao.UsuarioDAO;
-import tierraMedia.TipoAtraccion;
-import tierraMedia.Usuario;
+
 
 public class App {
 
@@ -12,16 +11,9 @@ public class App {
 		UsuarioDAO usuarioDAO = DAOFactory.getUsuarioDAO();
 		
 		System.out.println("----------------------------------");
-		Usuario as = new Usuario(9, "as", 10, 8, TipoAtraccion.AVENTURA);
 		
-		usuarioDAO.insert(as);
 		System.out.println(usuarioDAO.findAll());
 		System.out.println(usuarioDAO.countAll());
-		
-		usuarioDAO.delete(as);
-		System.out.println(usuarioDAO.findAll());
-		System.out.println(usuarioDAO.countAll());		
-		
 		
 		AtraccionDAO atraccionDAO = DAOFactory.getAtraccionDAO();
 		
