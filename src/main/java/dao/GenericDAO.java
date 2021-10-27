c918114ea975b22ffb2c24360599bfa04460f279
Virginia <virginiapiazza19@gmail.com>
2021-10-26 22:11:41 -0300

    Eliminé el método DELETE, comenté el de INSERT(Iriía en itinerario?)

--- src/main/java/dao/GenericDAO.java
+++ src/main/java/dao/GenericDAO.java
@@ -6,7 +6,6 @@ public interface GenericDAO<T> {
 
 	public List<T> findAll();
 	public int countAll();
-	public int insert(T t);
+	//public int insert(T t);
 	public int update(T t);
-	public int delete(T t);
 }
