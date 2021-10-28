f4381b59ca7d24c9f8c6f3ff189ee353e53a3078
Virginia <virginiapiazza19@gmail.com>
2021-10-27 21:04:29 -0300

    Se eliminan pruebas y se agregan las de promocion

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -2,22 +2,28 @@ package app;
 
 import dao.AtraccionDAO;
 import dao.DAOFactory;
+import dao.PromocionDAO;
 import dao.UsuarioDAO;
-
 
 public class App {
 
 	public static void main(String[] args) {
 		UsuarioDAO usuarioDAO = DAOFactory.getUsuarioDAO();
+
+		System.out.println("----------------------------------");
+
+		System.out.println(usuarioDAO.findAll());
+				
+		System.out.println("----------------------------------");
+
+		AtraccionDAO atraccionDAO = DAOFactory.getAtraccionDAO();
+
+		System.out.println(atraccionDAO.findAll());
 		
 		System.out.println("----------------------------------");
-		
-		System.out.println(usuarioDAO.findAll());
-		System.out.println(usuarioDAO.countAll());
-		
-		AtraccionDAO atraccionDAO = DAOFactory.getAtraccionDAO();
-		
-		System.out.println(atraccionDAO.findAll());
-		System.out.println(atraccionDAO.countAll());
+
+		PromocionDAO promocionDAO = DAOFactory.getPromocionDAO();
+
+		System.out.println(promocionDAO.findAll());
 	}
 }
