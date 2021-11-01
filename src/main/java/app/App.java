50ac915649566c48e76a3f8709ebbe3418518c83
Virginia Piazza <virginiapiazza19@gmail.com>
2021-11-01 20:15:25 -0300

    Se deja solo la prueba del programa

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -9,6 +9,7 @@ import daos.DAOFactory;
 import daos.ItinerarioDAO;
 import daos.PromocionDAO;
 import daos.UsuarioDAO;
+import excepciones.NoHayCupoException;
 import tierraMedia.Atraccion;
 import tierraMedia.Itinerario;
 import tierraMedia.Producto;
@@ -18,29 +19,12 @@ import tierraMedia.Usuario;
 
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoHayCupoException {
 		
-		UsuarioDAO usuarioDAO = DAOFactory.getUsuarioDAO();
-		System.out.println(usuarioDAO.findAll());
-		System.out.println("----------------------------------");		
-		
-		AtraccionDAO atraccionDAO = DAOFactory.getAtraccionDAO();
-		List<Atraccion> atracciones = atraccionDAO.findAll(); 
-		System.out.println(atracciones);
-		System.out.println("----------------------------------");
-
-		PromocionDAO promocionDAO = DAOFactory.getPromocionDAO();
-		System.out.println(promocionDAO.findAllPromo(atracciones));
-		System.out.println("----------------------------------");
-
-		ItinerarioDAO itinerarioDAO = DAOFactory.getItinerarioDAO();
-		List<Producto> productos = new ArrayList<Producto>();
-		System.out.println(itinerarioDAO.findAll(2, productos));
-		System.out.println("----------------------------------");
-
 		ParqueTierra parque = new ParqueTierra();
 		System.out.println(parque.generarUsuarios());
 		System.out.println(parque.generarProductos());
+		parque.ofrecerServicios (parque.usuarios, parque.productos);
 		
 	}
 }
