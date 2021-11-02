28a4f6cd3ab2e386a2ecf0c46118bfab1b4d3b87
Virginia Piazza <virginiapiazza19@gmail.com>
2021-11-02 00:43:03 -0300

    Se eliminan imports sin uso

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,21 +1,6 @@
 package app;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
-import daos.AtraccionDAO;
-import daos.DAOFactory;
-import daos.ItinerarioDAO;
-import daos.PromocionDAO;
-import daos.UsuarioDAO;
 import excepciones.NoHayCupoException;
-import tierraMedia.Atraccion;
-import tierraMedia.Itinerario;
-import tierraMedia.Producto;
-import tierraMedia.Promocion;
-import tierraMedia.Usuario;
-
 
 public class App {
 
